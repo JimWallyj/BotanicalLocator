0551b83d43f0156e9eb9df49622f326ff9ee8445
JimWallyj <jwalejko@me.com>
2026-08-13 19:36:31 -0700

    Initial Commit

---
+++ BotanicalLocator/BotanicalLocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BotanicalLocatorApp.swift
+//  BotanicalLocator
+//
+//  Created by JIM WALEJKO on 8/13/26.
+//
+
+import SwiftUI
+
+@main
+struct BotanicalLocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
